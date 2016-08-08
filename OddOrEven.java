2d093c46809e3d215c778aae577f3f05de8743ae
IndhumathiChandrasekaran <indhumathi.ccse@ksrce.ac.in>
2016-08-08 15:46:35 +0530

    Create OddOrEven.java

---
+++ OddOrEven.java
@@ -0,0 +1,12 @@
+public class OddOrEven{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+int num=s.nextInt();
+if(num%2==0){
+System.out.println("The number is Even");
+}
+else {
+System.out.println("The number is odd");
+}
+}
+}
