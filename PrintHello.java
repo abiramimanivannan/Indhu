19dbb9bcfe0a6d3b71abac682682c8c6b7c2eaa6
IndhumathiChandrasekaran <indhumathi.ccse@ksrce.ac.in>
2016-08-10 18:19:51 +0530

    Update and rename PrintHello to PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class PrintHello{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str=s.next();
+for(int i=0;i<5;i++)
+{
+System.out.println(str);
+}
+}
+}
