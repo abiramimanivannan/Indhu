c3ad05bee71b0dd0bb953ea0fbdfc40d0392a4e8
IndhumathiChandrasekaran <indhumathi.ccse@ksrce.ac.in>
2016-08-12 22:31:35 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Factorial{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+int i=s.nextInt();
+int r=1;
+for(int y=i;y<=1;y--){
+r*=y;
+}
+System.out.println(r);
+}}
