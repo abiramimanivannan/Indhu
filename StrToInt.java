a219262c6cb9546adae522c2db6a5cb9067f5531
IndhumathiChandrasekaran <indhumathi.ccse@ksrce.ac.in>
2016-08-16 15:11:55 +0530

    Create StrToInt.java

---
+++ StrToInt.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class StrToInt{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+String str=s.next();
+int num=Integer.parseInt(str);
+System.out.println(num);
+}
+}
